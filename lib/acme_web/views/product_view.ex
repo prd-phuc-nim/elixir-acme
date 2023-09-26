7deb1b43a5026a8075d8a3307ccf83c7a5ae6d17
prd-phuc-nim <phuc.nim@paradox.ai>
2023-09-26 15:11:41 +0700

    feat: init project

---
+++ lib/acme_web/views/product_view.ex
@@ -0,0 +1,37 @@
+defmodule AcmeWeb.ProductView do
+  use AcmeWeb, :view
+  alias AcmeWeb.ProductView
+
+  def render("index.json", %{products: products}) do
+    %{data: render_many(products, ProductView, "product.json")}
+  end
+
+  def render("show.json", %{product: product}) do
+    %{data: render_one(product, ProductView, "product.json")}
+  end
+
+  def render("product.json", %{product: product}) do
+    %{
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      quantity: product.quantity,
+      unit_cost: product.unit_cost
+    }
+  end
+
+  def render("total_inventory_cost.json", %{product: product, total_inventory_cost: total_inventory_cost}) do
+    %{
+      data: %{
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        category: product.category,
+        quantity: product.quantity,
+        unit_cost: product.unit_cost,
+        total_inventory_cost: total_inventory_cost
+      }
+    }
+  end
+end
